b57a979a8f3577b3574ab754350674ba2f96fc23
Dan Jackson <danjackson@hastingsdirect.com>
2019-05-22 12:04:25 +0100

    Update the webpack.config.js file to set the test rule to be only 'js'

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 
-ReactDOM.render(
-    <h1>Hello, world!</h1>,
+render(
+	<h1>Hello World</h1>,
     document.getElementById('app')
 );
