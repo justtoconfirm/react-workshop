1838f87c15207e66b58c9db3f30712677c747bc6
Dan Jackson <danjackson@hastingsdirect.com>
2019-06-04 22:45:50 +0100

    Setup of Routes and Links

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,10 +1,44 @@
 import React from 'react'
+import { Link, Route } from 'react-router-dom'
+// import { Link, Route, Switch } from 'react-router-dom'
 
-import Button from './presentational/atoms/button/Button'
+// import Button from './presentational/atoms/button/Button'
+
+/* Home component */
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
+
+/* Category component */
+const Category = () => (
+  <div>
+    <h2>Category</h2>
+  </div>
+)
+
+/* Products component */
+const Products = () => (
+  <div>
+    <h2>Products</h2>
+  </div>
+)
 
 const App = () => {
   return (
-    <Button theme='primary' isFullWidth>Test Button</Button>
+  /* <Button theme='primary' isFullWidth>Test Button</Button> */
+  /* Route components are rendered. if the path prop matches the current URL */
+
+    <div>
+      <Link to='/'>Home</Link>
+      <Link to='/category'>Category</Link>
+      <Link to='/products'>Products</Link>
+
+      <Route path='/' component={Home} />
+      <Route path='/category' component={Category} />
+      <Route path='/products' component={Products} />
+    </div>
   )
 }
 
