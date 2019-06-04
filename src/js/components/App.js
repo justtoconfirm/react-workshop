c40f84ab2293a1c2ea38ae5cd4ec7f003927fc8b
Dan Jackson <danjackson@hastingsdirect.com>
2019-06-05 00:03:57 +0100

    Switch component applied to allow 404 routing

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,21 +1,25 @@
 import React from 'react'
-import { Link, Route } from 'react-router-dom'
+import { Link, Route, Switch } from 'react-router-dom'
 
 import Home from './presentational/templates/Home'
 import PageTwo from './presentational/templates/PageTwo'
 import PageThree from './presentational/templates/PageThree'
+import NotFound from './presentational/templates/NotFound'
 
 const App = () => {
   return (
-    <div>
+    <React.Fragment>
       <Link to='/'>Home</Link>
       <Link to='/pagetwo'>Page Two</Link>
       <Link to='/pagethree'>Page Three</Link>
 
-      <Route exact path='/' component={Home} />
-      <Route path='/pagetwo' component={PageTwo} />
-      <Route path='/pagethree' component={PageThree} />
-    </div>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/pagetwo' component={PageTwo} />
+        <Route path='/pagethree' component={PageThree} />
+        <Route component={NotFound} />
+      </Switch>
+    </React.Fragment>
   )
 }
 
