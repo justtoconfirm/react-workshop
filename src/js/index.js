45bc9d9cab3c1e910ff3ae4dd87eacf110feda59
Dan Jackson <danjackson@hastingsdirect.com>
2019-06-04 22:36:48 +0100

    Import BrowserRouter from react-router-dom

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,11 @@ import { render } from 'react-dom'
 
 import App from './components/App'
 
+import { BrowserRouter as Router } from 'react-router-dom'
+
 render(
-  <App />,
+  <Router>
+    <App />
+  </Router>,
   document.getElementById('app')
 )
