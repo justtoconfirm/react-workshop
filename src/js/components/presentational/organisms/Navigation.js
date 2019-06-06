73b65f501652f24dbf3d4008a587ac2ca2a52fde
Dan Jackson <danjackson@hastingsdirect.com>
2019-06-06 08:11:12 +0100

    HTML5 nav element applied to the Navigation component as a wrapper

--- src/js/components/presentational/organisms/Navigation.js
+++ src/js/components/presentational/organisms/Navigation.js
@@ -3,9 +3,11 @@ import { Link } from 'react-router-dom'
 
 const Navigation = () => (
   <React.Fragment>
-    <Link to='/'>Home</Link>
-    <Link to='/pagetwo'>Page Two</Link>
-    <Link to='/pagethree'>Page Three</Link>
+    <nav>
+      <Link to='/'>Home</Link>
+      <Link to='/pagetwo'>Page Two</Link>
+      <Link to='/pagethree'>Page Three</Link>
+    </nav>
   </React.Fragment>
 )
 
